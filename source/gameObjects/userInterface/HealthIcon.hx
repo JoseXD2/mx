b50a409fc906bdac592f1d95f35dea90f8699141
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-10-05 15:02:46 -0300

    Update HealthIcon.hx

--- source/gameObjects/userInterface/HealthIcon.hx
+++ source/gameObjects/userInterface/HealthIcon.hx
@@ -27,7 +27,7 @@ class HealthIcon extends FlxSprite
 			trimmedCharacter = trimmedCharacter.substring(0, trimmedCharacter.indexOf('-'));
 
 		var iconPath = char;
-		while (!FileSystem.exists(Paths.image('icons/icon-' + iconPath))) {
+		while (!Assets.exists(Paths.image('icons/icon-' + iconPath))) {
 			if (iconPath != trimmedCharacter)
 				iconPath = trimmedCharacter;
 			else
